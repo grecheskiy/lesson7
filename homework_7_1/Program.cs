e59b32f6add89c2117d0d2493971f80e632c9078
grecheskiy <grecheskiy@rambler.ru>
2023-02-05 00:33:37 +0300

    add homework_7_1

---
+++ homework_7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿//Написать программу, которая обменивает элементы первой строки и последней строки.
+Console.Write("Enter number of lines: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Enter number of columns: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+int[,] matrix= new int[n, m];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+ExchangeMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+
+void FillMatrix(int[,] matr)
+{for (int i=0; i<matr.GetLength(0); i++)
+{for (int j=0; j<matr.GetLength(1); j++)
+{matr[i, j] = new Random().Next(0,100);}}}
+
+void PrintMatrix(int[,] matr)
+{for (int i=0; i<matr.GetLength(0); i++)
+{for (int j=0; j<matr.GetLength(1); j++)
+{Console.Write($"{matr[i, j]} ");}
+Console.WriteLine();}}
+
+void ExchangeMatrix(int[,] matr)
+{int temp = 0;
+for (int i=0; i<matr.GetLength(1); i++)
+{temp = matr[matr.GetLength(0)-1, i];
+matr[matr.GetLength(0)-1, i] = matr[0, i];
+matr[0, i] = temp;}}
