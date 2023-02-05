ab9430d4c5adcd4a2ec6cd2c1e954f22794803c7
grecheskiy <grecheskiy@rambler.ru>
2023-02-05 22:48:49 +0300

    add  homework_7_3

---
+++ homework_7_3/Program.cs
@@ -0,0 +1,30 @@
+﻿//В прямоугольной матрице найти строку с наименьшей суммой элементов.
+int[,] matrix = new int[5, 9];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine("line with minsum: " + MinSumMatrix(matrix));
+
+void FillMatrix(int[,] matr)
+{for (int i = 0; i < matr.GetLength(0); i++)
+{for (int j = 0; j < matr.GetLength(1); j++)
+{matr[i, j] = new Random().Next(0,100);}}}
+
+void PrintMatrix(int[,] matr)
+{for (int i = 0; i < matr.GetLength(0); i++)
+{for (int j = 0; j < matr.GetLength(1); j++)
+{Console.Write($"{matr[i, j]} ");}
+Console.WriteLine();}}
+
+int MinSumMatrix(int[,] matr)
+{int MinSum = 0;
+int temp = 0;
+int num = 0;
+for (int i = 0; i < matr.GetLength(0); i++)
+{for (int j = 0; j < matr.GetLength(1); j++)
+{MinSum += matr[0, j];
+temp += matr[i, j];
+if (temp < MinSum)
+{MinSum = temp;
+num = i;}}
+temp = 0;}
+return num;}
